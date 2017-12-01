965e82e3bc14cfd4ec76c24aeb3cde577f1a9546
littletry <1109394634@qq.com>
2017-12-01 22:53:45 +0800

    添加这些代码才能正常使用swagger

--- src/main/java/top/littletry/sharezone/controller/HelloWorldController.java
+++ src/main/java/top/littletry/sharezone/controller/HelloWorldController.java
@@ -3,6 +3,8 @@ package top.littletry.sharezone.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 /**
  * Created by Intellij Idea
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class HelloWorldController {
-    @RequestMapping(value = "/helloWorld")
+    @RequestMapping(value = "/helloWorld",method = RequestMethod.POST)
     public String helloWorld(Model model){
         model.addAttribute("message","Hello World!");
         return "welcome";

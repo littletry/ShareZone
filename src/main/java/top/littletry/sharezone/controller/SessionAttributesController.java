0f4cec506a2c7cff0dda86fa796aae39c17c1701
littletry <1109394634@qq.com>
2017-12-03 18:46:55 +0800

    @SessionAttributes注解，指定Model中的哪些属性需要转存到HttpSession对象当中

--- src/main/java/top/littletry/sharezone/controller/SessionAttributesController.java
+++ src/main/java/top/littletry/sharezone/controller/SessionAttributesController.java
@@ -4,7 +4,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;

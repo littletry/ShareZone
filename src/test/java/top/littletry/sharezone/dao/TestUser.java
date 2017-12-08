2f78048c3dc13fc5007d6fe5c32bfecb9d5c1819
littletry <1109394634@qq.com>
2017-12-08 20:17:33 +0800

    注释mybatis逆向工程生成的代码，后面手写SQL

--- src/test/java/top/littletry/sharezone/dao/TestUser.java
+++ src/test/java/top/littletry/sharezone/dao/TestUser.java
@@ -24,13 +24,14 @@ import java.util.UUID;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:spring-mybatis.xml")
 public class TestUser {
-    @Autowired
+    //采用mybatis逆向工程生成的代码来使用的，现已注释，准备后面手写SQL，不依赖自动生成
+    /*@Autowired
     private UserMapper userMapper;
 
-    /**
+    *//**
      * 通过id查询用户
      * @throws Exception
-     */
+     *//*
     @Test
     public void SelectUserById() throws Exception{
         User user = userMapper.selectByPrimaryKey("1");
@@ -69,18 +70,18 @@ public class TestUser {
     }
     @Test
     public void updateUser() throws Exception{
-        /**
+        *//**
          * 更新要先查出来再更新
-         */
+         *//*
         UserQuery userQuery = new UserQuery();
         userQuery.createCriteria().andUserNameEqualTo("王五");
         List<User> users = userMapper.selectByExample(userQuery);
         User user = users.get(0);
         user.setUserName("小李子");
         userMapper.updateByPrimaryKey(user);
-        /**
+        *//**
          * 查询出刚插入的记录
-         */
+         *//*
         UserQuery userQuery1 = new UserQuery();
         userQuery1.createCriteria().andUserNameLike("%小%");
         List<User> userList = userMapper.selectByExample(userQuery1);
@@ -95,5 +96,5 @@ public class TestUser {
         userQuery.createCriteria().andUserNameEqualTo("小张");
         userMapper.deleteByExample(userQuery);
     }
-
+*/
 }

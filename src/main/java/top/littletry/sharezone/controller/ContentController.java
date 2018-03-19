e080bbd5b32cb369b600a04abc1bfacfde2b3cd4
LittleTry <ht@yuancore.com>
2018-03-20 00:24:47 +0800

    添加文件上传，不完整不规范，待整理

--- src/main/java/top/littletry/sharezone/controller/ContentController.java
+++ src/main/java/top/littletry/sharezone/controller/ContentController.java
@@ -5,13 +5,14 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import top.littletry.sharezone.common.dto.RestResponse;
 import top.littletry.sharezone.model.Content;
 import top.littletry.sharezone.service.ContentService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
 
 /**
  * Created by LittleTry
@@ -29,7 +30,7 @@ public class ContentController {
 
     @ApiOperation(value = "提交分享内容",notes = "提交分享内容")
     @ResponseBody
-    @RequestMapping(value = "/_commit", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/commit", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
     public RestResponse<String> commit(@RequestBody Content content) {
 
         boolean checkContent = contentService.insertContent(content);
@@ -39,5 +40,60 @@ public class ContentController {
             return RestResponse.failed("发表失败，内容包含敏感词汇");
         }
     }
+    @ApiOperation(value = "上传单个文件",notes = "上传单个文件")
+    @ResponseBody
+    @RequestMapping(value = "/upload", method = RequestMethod.POST, produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String singleUpload(
+            HttpServletRequest request,
+            @RequestParam("userId") String userId,
+            @RequestParam("file")MultipartFile file) throws Exception {
+        System.out.println(userId);
+        //如果文件不为空，写入上传路径
+        if (!file.isEmpty()){
+            //上传文件路径
+            String path = request.getServletContext().getRealPath("/images/");
+            //上传文件名
+            String filename = file.getOriginalFilename();
+            File filepath = new File(path,filename);
+            //判断路径是否存在，如果不存在就创建一个
+            if (!filepath.getParentFile().exists()){
+                filepath.getParentFile().mkdirs();
+            }
+            //将上传文件保存到一个目标文件当中
+            file.transferTo(new File(path+File.separator+filename));
+            return "success";
+        }else {
+            return "error";
+        }
 
+    }
+    @ApiOperation(value = "上传多个文件",notes = "上传多个文件")
+    @ResponseBody
+    @RequestMapping(value = "/_upload", method = RequestMethod.POST, produces = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String moreUpload(
+            HttpServletRequest request,
+            @RequestParam("userId") String userId,
+            @RequestParam("file")MultipartFile[] files) throws Exception {
+        System.out.println(userId);
+        //如果文件不为空，写入上传路径
+        for (MultipartFile mul: files) {
+            if (!mul.isEmpty()){
+                //上传文件路径
+                String path = request.getServletContext().getRealPath("/images/");
+                //上传文件名
+                String filename = mul.getOriginalFilename();
+                File filepath = new File(path,filename);
+                //判断路径是否存在，如果不存在就创建一个
+                if (!filepath.getParentFile().exists()){
+                    filepath.getParentFile().mkdirs();
+                }
+                //将上传文件保存到一个目标文件当中
+                mul.transferTo(new File(path+File.separator+filename));
+                return "success";
+            }else {
+                return "error";
+            }
+        }
+    return "null";
+    }
 }

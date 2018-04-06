3857beaf04b3a2a727c12dcedb83a54a95386642
LittleTry <ht@yuancore.com>
2018-04-06 12:43:06 +0800

    mybatis-plus默认的生成代码的代码

---
+++ src/main/java/top/littletry/sharezone/common/GeneratorTest.java
@@ -0,0 +1,62 @@
+package top.littletry.sharezone.common;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.rules.DbType;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import org.junit.Test;
+
+/**
+ * @author ht
+ */
+public class GeneratorTest {
+    @Test
+    public void generateCode() {
+        String packageName = "top.littletry.sharezone";
+        //user -> UserService, 设置成true: user -> IUserService
+        boolean serviceNameStartWithI = true;
+        generateByTables(serviceNameStartWithI, packageName, "user", "content");
+    }
+    private void generateByTables(boolean serviceNameStartWithI, String packageName, String... tableNames) {
+        GlobalConfig config = new GlobalConfig();
+        String dbUrl = "jdbc:mysql://localhost:3306/sharezone";
+        DataSourceConfig dataSourceConfig = new DataSourceConfig();
+        dataSourceConfig.setDbType(DbType.MYSQL)
+                .setUrl(dbUrl)
+                .setUsername("root")
+                .setPassword("pass1234")
+                .setDriverName("com.mysql.jdbc.Driver");
+        StrategyConfig strategyConfig = new StrategyConfig();
+        strategyConfig
+                .setCapitalMode(true)
+                .setEntityLombokModel(false)
+                .setDbColumnUnderline(true)
+                .setNaming(NamingStrategy.underline_to_camel)
+                //修改替换成你需要的表名，多个表名传数组
+                .setInclude(tableNames);
+        config.setActiveRecord(false)
+                .setAuthor("littletry")
+                .setOutputDir("E:\\codeDen")
+                .setFileOverride(true);
+        if (!serviceNameStartWithI) {
+            config.setServiceName("%sService");
+        }
+        new AutoGenerator().setGlobalConfig(config)
+                .setDataSource(dataSourceConfig)
+                .setStrategy(strategyConfig)
+                .setPackageInfo(
+                        new PackageConfig()
+                                .setParent(packageName)
+                                .setController("controller")
+                                .setEntity("model")
+                                .setMapper("dao")
+                                .setXml("dao.xml")
+                ).execute();
+    }
+    private void generateByTables(String packageName, String... tableNames) {
+        generateByTables(true,packageName,tableNames);
+    }
+}

ccebfd8001164a36bb06bd83270e663ad71c8f19
LittleTry <ht@yuancore.com>
2018-04-16 19:31:45 +0800

    登录页面基本完成

---
+++ src/main/webapp/web/login.js
@@ -0,0 +1,39 @@
+var login = avalon.define({
+    $id: "login",
+    loginName: "",
+    password: "",
+    init: function () {
+       
+    },
+    enter:function (e) {
+        if (e.which == 13){
+            login.userLogin();
+        }
+    },
+    userLogin: function () {
+        console.log('用户名',login.loginName);
+        console.log('密码',login.password);
+        if (login.loginName.trim() === '' || login.loginName === null) {
+            layer.alert('输入账户为空');
+            return;
+        }
+        if (login.password.trim() === '' || login.password === null) {
+            layer.alert('输入密码为空');
+        }
+        var psd = md5(login.password);
+        $.ajax({
+            type: 'POST',
+            url: '/ShareZone/user/login?loginName=' + login.loginName + '&password=' + psd,
+            success: function (result) {
+                console.log(result);
+                if (result.code === 0) {
+                    layer.alert(result.message);
+                } else if (result.code === 501) {
+                    layer.alert(result.message);
+                }
+            }
+        });
+    }
+});
+var init = function () {
+}();

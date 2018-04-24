d8e75e2820c345580a6a0d37949ab9edc1fb123e
LittleTry <ht@yuancore.com>
2018-04-24 22:14:11 +0800

    用户分享内容，管理员审核实现

--- src/main/webapp/web/list_content.js
+++ src/main/webapp/web/list_content.js
@@ -8,11 +8,47 @@ var listContent = avalon.define({
             success: function (result) {
                 if (result.code === 0) {
                     listContent.contentList = result.content;
-                    console.log(result);
                 } else {
                     layer.alert("系统中没有分享内容");
                 }
             }
+        });
+    },
+    changeContent: function (contentId) {
+        layer.confirm('确认通过该分享内容吗？', {
+            btn: ['通过', '不通过']
+        }, function(){
+            $.ajax({
+                type: "POST",
+                url: "/ShareZone/content/change?contentId=" + contentId + "&checkPublish=1",
+                success: function (result) {
+                    if (result.code === 0) {
+                        layer.alert(result.message, function () {
+                            location.replace(location.href);
+                        });
+                    } else {
+                        layer.alert(result.message, function () {
+                            location.replace(location.href);
+                        });
+                    }
+                }
+            });
+        }, function(){
+            $.ajax({
+                type: "POST",
+                url: "/ShareZone/content/change?contentId=" + contentId + "&checkPublish=2",
+                success: function (result) {
+                    if (result.code === 0) {
+                        layer.alert(result.message, function () {
+                            location.replace(location.href);
+                        });
+                    } else {
+                        layer.alert(result.message, function () {
+                            location.replace(location.href);
+                        });
+                    }
+                }
+            });
         });
     }
 
